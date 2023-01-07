e62e8f229d6a5e380055efca994b2925bf200039
zxf4399 <zxf4399@gmail.com>
2023-01-07 19:55:54 +0800

    feat: add /api/whoami

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-// index.js
-// where your node app starts
+const express = require("express");
+const cors = require("cors");
 
-// init project
-require("dotenv").config();
-var express = require("express");
-var app = express();
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors());
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.acceptsLanguages(),
+    software: req.get("User-Agent"),
+  });
 });
 
-// listen for requests :)
-var listener = app.listen(3000, function () {
+const listener = app.listen(3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
